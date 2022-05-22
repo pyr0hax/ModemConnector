b6285195274938bb611882f2296c8f14b904cf1f
Jaco van Zyl <jvanzyl5@outlook.com>
2022-05-22 22:23:15 +0200

    Beautified

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import serial
 import configparser
 from datetime import datetime
 
-date_time = datetime.now().strftime("%m/%d/%Y - %H:%M:%S")
+date_time = datetime.now().strftime('%m/%d/%Y - %H:%M:%S')
 
 try:
     parser = configparser.ConfigParser()
     parser.read('cfg.ini')
-    SERIALPORT = parser.get('config','SERIALPORT')
-    BAUDRATE = parser.get('config','BAUDRATE')
-    
-except Exception as e:
+    SERIALPORT = parser.get('config', 'SERIALPORT')
+    BAUDRATE = parser.get('config', 'BAUDRATE')
+except Exception, e:
+
     with open('logs.txt', 'a') as f:
         f.write(date_time + ' - Config file does not exist: ' + str(e))
     with open('cfg.ini', 'a') as f:
@@ -18,7 +20,6 @@ except Exception as e:
 SERIALPORT = COM2
 BAUDRATE = 1200''')
         raise e
-        
 
 ser = serial.Serial(SERIALPORT, int(BAUDRATE))
 ser.bytesize = serial.EIGHTBITS
@@ -32,11 +33,11 @@ ser.writeTimeout = 0
 
 try:
     ser.isOpen()
-except Exception as e:
-    print("Exception: Opening serial port: " + str(e))
+except Exception, e:
+    print 'Exception: Opening serial port: ' + str(e)
 while True:
-    print("waiting for modem")
-    ser.write("ats0=1\r\n".encode('ascii'))
+    print 'waiting for modem'
+    ser.write('ats0=1\r\n'.encode('ascii'))
     response = ser.readline().decode('ascii')
     if response == 'RING':
-        ser.write("OK\r\n".encode('ascii'))
+        ser.write('OK\r\n'.encode('ascii'))
